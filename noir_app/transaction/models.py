7a4de9ef66af62a3c097236ee49dacba09746ff6
pochangl <pochanglee2008@gmail.com>
2016-05-06 21:55:08 +0800

    er diagram updated, transaction model update

--- noir_app/transaction/models.py
+++ noir_app/transaction/models.py
@@ -3,13 +3,19 @@ from utils.models import TimeStampModel
 from account.models import Employee
 # Create your models here.
 
-
-class PayCheck(TimeStampModel):
-    employee = models.ForeignKey(Employee, related_name='paycheck_employee_id')
-    amount = models.CharField(max_length=128)
-    reason_code = models.CharField(max_length=128)
-    reason = models.CharField(max_length=128)
+class Transaction(TimeStampModel):
+    amount = models.IntegerField()
+    comment = models.TextField()
 
 
-    def __str__(self):
-        return id
+class PayCheck(Transaction):
+    employee = models.ForeignKey(Employee, related_name='paychecks')
+
+
+class Debt(Transaction):
+    employee = models.ForeignKey(Employee, related_name='debts')
+
+
+class Receivable(Transaction):
+    client = models.ForeignKey(Client)
+
