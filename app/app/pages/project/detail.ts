da2fcc546ec13fac0e6620bd0b21b997789013a9
orangeyogurt <qoodydy@gmail.com>
2016-07-24 23:47:17 +0800

    取消checkbox計數功能,待連結database再測試

--- app/app/pages/project/detail.ts
+++ app/app/pages/project/detail.ts
@@ -1,27 +1,32 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
 	templateUrl: 'build/pages/project/detail.html'
 })
 
 export class ProjectDetailPage {
-	number_count = 3;
-	
+    public dataObject;
+	number_count = 0;
+    
 	project;
     selectedItem: any;
-	employees = [{name: "Employee 1"}, {name: "Employee 2"}, {name: "Employee 3"}];
+	employees = [{name: "Employee 1", selected: false}, {name: "Employee 2", selected: false}, {name: "Employee 3", selected: false}];
 
-	constructor(params: NavParams) {
+	constructor(
+		params: NavParams,
+		private http: Http
+	){
 		this.project = params.data.project;
 	}
 	
-
+	numberCount(){
+		return this.number_count;
+	}
     
-	click(employee,number_count){
+	ngModel(employee){
 	    employee.selected = !employee.selected;
-        number_count++;
-        alert(number_count);
-	};
+	}
 }
