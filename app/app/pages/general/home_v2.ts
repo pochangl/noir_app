0309316ab5e4a4b2c128048a0bb7f7a38eb851ec
orangeyogurt <qoodydy@gmail.com>
2016-07-27 23:31:03 +0800

    於project的工地列表新增人數資訊

--- app/app/pages/general/home_v2.ts
+++ app/app/pages/general/home_v2.ts
@@ -11,13 +11,17 @@ import {DayOffPage} from '../schedule/dayoff';
 export class HomePageV2 {  
 	//slide page of project
   	projects = [];
+  	number_count = 0;
 	constructor(private nav: NavController){
 		this. projects = [{"id":1,"name":"工地 1","number_needed":"1"},{"id":2,"name":"工地 2","number_needed":"2"},{"id":3,"name":"工地 3","number_needed":"3"}];
+	}
+	numberCount(){
+		return this.number_count;
 	}
 	click_of_project(project){
 		this.nav.push(ProjectDetailPage, { project: project });
 	}
-		
+	
 		
 	//slide page of transaction
 	employees = [{name: "Employee 1", id: 1}, {name: "Employee 2", id:2}, {name: "Employee 3", id: 3}];
