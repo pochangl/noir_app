c66a741538a88fcaa2c0c6f99e07e152b60be09b
orangeyogurt <qoodydy@gmail.com>
2016-07-27 22:30:53 +0800

    在menu新增home_v2,將原本的home改為slide畫面

--- app/app/app.ts
+++ app/app/app.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {SignInPage} from './pages/general/sign_in';
+import {HomePageV2} from './pages/general/home_v2';
 import {HomePage} from './pages/general/home';
 import {ProjectListPage} from './pages/project/list';
 import {PayCheckListPage} from './pages/transaction/list';
@@ -27,6 +28,7 @@ class MyApp {
     // set our app's pages
     this.pages = [    
       { title: '使用者登錄', component: SignInPage },
+      { title: '主選單_v2', component: HomePageV2 },
       { title: '主選單', component: HomePage },
       { title: '選擇工地', component: ProjectListPage},
       { title: '請假', component: DayOffListPage},
---
+++ app/app/pages/general/home_v2.ts
@@ -0,0 +1,49 @@
+import {Component} from '@angular/core';
+import {Loading, NavController, NavParams} from 'ionic-angular';
+import {ProjectDetailPage} from '../project/detail'
+import {HomePage} from '../general/home';
+import {PayCheckPage} from '../transaction/paycheck';
+import {DayOffPage} from '../schedule/dayoff';
+
+@Component({
+    templateUrl: 'build/pages/general/home_v2.html'
+})
+export class HomePageV2 {  
+	//slide page of project
+  	projects = [];
+	constructor(private nav: NavController){
+		this. projects = [{"id":1,"name":"工地 1","number_needed":"1"},{"id":2,"name":"工地 2","number_needed":"2"},{"id":3,"name":"工地 3","number_needed":"3"}];
+	}
+	click_of_project(project){
+		this.nav.push(ProjectDetailPage, { project: project });
+	}
+		
+		
+	//slide page of transaction
+	employees = [{name: "Employee 1", id: 1}, {name: "Employee 2", id:2}, {name: "Employee 3", id: 3}];
+	click_of_transaction(employee){
+		this.nav.push(PayCheckPage, {employee: employee});
+	}
+	
+	
+	//slide page of schedule
+	//employees = [{name: "Employee 1", id: 1}, {name: "Employee 2", id:2}, {name: "Employee 3", id: 3}];
+	click_of_schedule(employee){
+		this.nav.push(DayOffPage, {employee: employee});
+	}
+	
+	
+	//slide page of sign in
+	presentLoading() {
+		let loading = Loading.create({
+			content: "Please wait...",
+			duration: 3000,
+			dismissOnPageChange: true
+		});
+		this.nav.present(loading);
+	}
+	sign_in(){
+		this.presentLoading();
+		this.nav.push(HomePage);
+	}
+}
