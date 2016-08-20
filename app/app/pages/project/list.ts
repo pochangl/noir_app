b2602584983ec1ef5991efec89d153f4f3d88368
Pochang Lee <webmaster@classfigure.com>
2016-08-20 12:45:49 +0800

    first attempt to implementate API as an alternative to Http

--- app/app/pages/project/list.ts
+++ app/app/pages/project/list.ts
@@ -1,26 +1,27 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-import {Http} from '@angular/http';
+import {Api} from '../../providers/api/api'
 import {ProjectDetailPage} from './detail'
+import { Http } from '@angular/http';
 
 @Component({
-	templateUrl: 'build/pages/project/list.html'
+    templateUrl: 'build/pages/project/list.html',
+    providers: [Api]
 })
 
 export class ProjectListPage {
-	projects: any;
-	constructor(private nav: NavController, private http: Http){
-		this.projects = [];
-		this.http.get(
-			'/api/v1/project/?format=json'
-		).map(response => response.json()
-		).subscribe((data)=>{
-			this.projects = data.objects;
-		});
-		
+    projects: any;
+    constructor(private nav: NavController, private http: Api) {
+        this.projects = [];
+        this.http.get(
+            '/api/v1/project/?format=json'
+            ).map(response => response.json()
+                ).subscribe((data) => {
+                    this.projects = data.objects;
+                });
 
-	}
-	click(project){
-		this.nav.push(ProjectDetailPage, {project: project});
-	}
+    }
+    click(project) {
+        this.nav.push(ProjectDetailPage, { project: project });
+    }
 }
