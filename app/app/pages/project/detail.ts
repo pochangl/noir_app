e6bf5d3d2b659c143293308e2214f61f44ecfa6d
orangeyogurt <qoodydy@gmail.com>
2016-10-02 04:20:18 +0800

    改用employee_project後,尚未能解決http.get取得之array如何抓出id之問題

--- app/app/pages/project/detail.ts
+++ app/app/pages/project/detail.ts
@@ -16,6 +16,8 @@ export class ProjectDetailPage {
 	employee: any;
 	employee_projects: any;
 	assignments: any;
+	ind_assignment: any;
+	ind_assignment_id: any;
 
 	constructor(
 				private nav: NavController,
@@ -27,6 +29,7 @@ export class ProjectDetailPage {
 		this.employee = Object;
 		this.employee_projects = [];
 		this.assignments = [];
+		this.ind_assignment = [];
 
 		this.http.get({
 			resource_name: "employee_project",
@@ -42,23 +45,28 @@ export class ProjectDetailPage {
 		this.syncAssignments();
 	}
 
-	indicateEmployeeProject (assignment){
-    return this.http.get({
-					resource_name: "employee_project",
-					urlParams: {
-						"employee": assignment.employee,
-						"project": assignment.project
-					}
-				}).map(
-					response => response.text()
-				).subscribe(
-		      data => this.employee_projects = data,
-		      err => console.error(err),
-					() => console.log(this.employee_projects)
-    		);
+
+	indicateAssignmentId(employee_project) {
+    this.http.get({
+			resource_name: "assignment",
+			urlParams: {
+				"employee": employee_project.employee,
+				"project": employee_project.project
+			}
+		}).map(
+			response => response.json()
+		).subscribe(
+			//若用map格式為list,抓不到id;
+			//若不用map格式為response
+			//Q:如何以dictionary方式抓出assignment.id？
+      data => this.ind_assignment_id = data.objects,
+      err => console.error(err),
+			() => console.log(this.ind_assignment_id)
+		);
 	}
 
-  syncAssignments(){
+
+  syncAssignments() {
 		return this.http.get({
 			resource_name: "assignment",
 		}).map(
@@ -68,22 +76,69 @@ export class ProjectDetailPage {
 		});
 	}
 
-	count(){
+	putAssignment(ind_assignment) {
+		console.log(this.ind_assignment_id);
+		//ind_assignment_id為undefined,造成data lost
+		/*
+		return this.http.put({
+	    resource_name: "assignment",
+			id: ind_assignment.id
+	  }, ind_assignment
+		).subscribe(
+			data => {
+				// assignment = data.objects;
+			},
+			error => {
+				console.error(error);
+				this.syncAssignments();
+			}
+		)
+		*/
+	}
+
+	postAssignment(ind_assignment) {
+
+	}
+
+	count() {
 		//return this.employees.filter(function(item){return !! item.selected;}).length;
 		return this.assignments.filter(item=>!!item.selected).length;
 	}
 
-	toggle(assignment){
-		assignment.selected = !assignment.selected;
+	toggle(employee_project) {
+		console.log(employee_project);
 		// get assginment from employee_project
-		// 修改detail版面,並增加employee_project的selected屬性
-		this.indicateEmployeeProject(assignment);
+		// indicate assignment by employee_porject
+		// 取消用函數，直接利用data跟error進行判斷
+		// ind_assignment若放在外面，則第一次按抓不到,第二次按才會抓到,原因不明
+		// this.indicateAssignment(employee_project);
+		this.http.get({
+			resource_name: "assignment",
+			id: this.indicateAssignmentId(employee_project)
+		}).map(
+			response => response.json()
+		).subscribe(
 			//success
+			data => {
+				this.ind_assignment = data;
 				// put assignment
+				this.putAssignment(this.ind_assignment);
+			},
+
 			//error subscribe data.meta.total_count = 0
-				// create new assignment
-					//success
+			err => {
+					if(err.meta.total_count = 0){
+						// create new assignment
+					  employee_project.selected = !employee_project.selected;
+					  this.postAssignment(this.ind_assignment);
+					}else{
+						//success
 						// put assignment
+						this.putAssignment(this.ind_assignment);
+					}
+			}
+			//() => console.log(this.ind_assignment)
+		);
 	}
 
 /*
