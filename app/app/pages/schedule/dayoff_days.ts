805193904356963b774accc49b2acc4002e0ea16
orangeyogurt <qoodydy@gmail.com>
2016-11-23 22:39:35 +0800

    dayoff days page增加新增請假紀錄的功能

--- app/app/pages/schedule/dayoff_days.ts
+++ app/app/pages/schedule/dayoff_days.ts
@@ -27,6 +27,24 @@ export class DayOffDaysPage {
     this.dayoffs.fetch();
   }
 	click(dayoff){
+		console.log(dayoff);
 		this.nav.push(DayOffDetailPage, {dayoff: dayoff});
+	}
+	new_record(){
+		//不知道為什麼月份會少一個月？
+		var today = new Date();
+		var curDate = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
+		var curTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		var initial_data = {
+			"employee": {"contact": {"id": this.employee.contact.id, "name": this.employee.contact.name}, "id": this.employee.id},
+									"id": 1,
+									"start_datetime": curDate + "T" + "08:00:00",
+									"start_date": curDate,
+									"start_time": "08:00:00",
+									"end_datetime": curDate + "T" + "17:00:00",
+									"end_date": curDate,
+									"end_time": "17:00:00"
+		};
+		this.nav.push(DayOffDetailPage, {dayoff: initial_data});
 	}
 }
